60bc4957c53866e0a959539c7728fae83be46b06
Filipe <filiperdacas@gmail.com>
2024-04-19 17:12:42 -0300

    inserted repository, controller e thymeleaf

--- src/main/java/com/convidados/controllers/ConvidadosController.java
+++ src/main/java/com/convidados/controllers/ConvidadosController.java
@@ -1,16 +1,25 @@
 package com.convidados.controllers;
 
+import com.convidados.repository.ConvidadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-//@RestController
-////@RequestMapping("/convidados")
+
 @Controller
 public class ConvidadosController {
+    @Autowired
+    private ConvidadoRepository convidadosRepository;
+
     @GetMapping("/convidados")
-    public String listar(){
-        return "ListaConvidados";
+    public ModelAndView listar(){
+        ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+
+        modelAndView.addObject("convidados", convidadosRepository.findAll());
+
+        return modelAndView;
     }
 }
---
+++ src/main/java/com/convidados/repository/ConvidadoRepository.java
@@ -0,0 +1,7 @@
+package com.convidados.repository;
+
+import com.convidados.model.Convidado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConvidadoRepository extends JpaRepository<Convidado, Long>{
+}
